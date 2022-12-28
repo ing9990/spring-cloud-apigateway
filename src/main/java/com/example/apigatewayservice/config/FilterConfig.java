d5c4f5a77af361cc4b512d261078942d383a8431
mail80782258 <mail80782258@gmail.com>
2022-12-28 10:17:55 +0900

    Initial commit

---
+++ src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -0,0 +1,25 @@
+package com.example.apigatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
+//@Configuration
+public class FilterConfig {
+
+    //@Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(route -> route.path("/first-service/**")
+                        .filters(filter -> filter
+                                .addRequestHeader("first-request", "first-request-header")
+                                .addResponseHeader("first-respose", "first-respose-header"))
+                        .uri("http://localhost:8081"))
+
+                .route(route -> route.path("/second-service/**")
+                        .filters(filter -> filter
+                                .addRequestHeader("second-request", "second-request-header")
+                                .addResponseHeader("second-respose", "second-respose-header"))
+                        .uri("http://localhost:8082"))
+                .build();
+    }
+}
